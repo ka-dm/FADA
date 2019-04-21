3091b374588ebb802d5af6d0d6d18330b88a7c70
kevindm <kevindm181998@gmail.com>
2019-04-20 19:10:07 -0500

    Algoritmo funcionando correctamente$

--- problema1Pd/problem1Pd.py
+++ problema1Pd/problem1Pd.py
@@ -1,7 +1,7 @@
-# Este Script esta optimizado para Python 3.x.x
 
 from operator import itemgetter
 import datetime
+import os
 
 def usoSalaCirugias(w, beneficio, M):
     """
@@ -48,12 +48,14 @@ def usoSalaCirugias(w, beneficio, M):
 
     return procEscogidos, (K[n][w])/2,'horas'
 
-def leeArchivo():
+def leeArchivo(rutaArchivo):
     global entrada
     
     #src = 'C:\\Users\\kevin\\Desktop\\proyectoFinalFADA\\problema1Pd\\entradas\\punto1PdEntrada1.txt'
-    src = 'C:\\Users\\kevin\\Desktop\\proyectoFinalFADA\\problema1Pd\\entradas\\punto1PdEntrada2.txt'
+    #src = 'C:\\Users\\kevin\\Desktop\\proyectoFinalFADA\\problema1Pd\\entradas\\punto1PdEntrada2.txt'
     #src = 'C:\\Users\\kevin\\Desktop\\proyectoFinalFADA\\problema1Pd\\entradas\\punto1PdEntrada3.txt'
+
+    src = rutaArchivo
 
     entrada = []
     archivoEntrada = open(src, 'r')
@@ -69,13 +71,16 @@ def leeArchivo():
     #printMatriz(entrada)
 
 def crearArchivoSalida(contenido):
-    src = 'C:\\Users\\kevin\\Desktop\\proyectoFinalFADA\\problema1Pd'
+    src = os.getcwd() #retorna la ruta actual de archivo .py
 
     archivoSalida = open(src + '\\punto1PdSalida.txt', 'w')
     archivoSalida.write(str(contenido[1]) + '\n')
     for i in range(len(contenido[0])):
         archivoSalida.write(str(contenido[0][i]) + '\n')
     archivoSalida.close()
+    print("#################################################")
+    print("# Se genero correctamente el archivo de salida. #")
+    print("#################################################")
 
 def printMatriz(a):
     #imprime una matriz
@@ -86,8 +91,7 @@ def printMatriz(a):
 def ordenarMatriz(a,colum):
     ##ordena una matriz de menor a mayor, recibe como argumentos una matriz 'a' y una columna 'colum', retorna una matriz ordena
     bco_x_des = sorted(a, key=itemgetter(colum-1))
-
-    printMatriz(bco_x_des)
+    #printMatriz(bco_x_des)
     return bco_x_des
 
 def diferenciaHora(hIniStr, hFinStr):
@@ -112,11 +116,8 @@ def strToDateObject(hora):
     hDateObject = datetime.datetime.strptime(hora, '%H:%M')
     return hDateObject 
 
-def noEstaSolapado(hFin,hIni):
-    #h1 = strToDateObject(hFin)
-    #h2 = strToDateObject(hIni)
-    #print('h1= ', h1, 'h2= ', h2)   
-    
+def noEstaSolapado(hFin,hIni): 
+    # Compara la hora de finalizacion con la hora de incio entre dos procesos 
     h1 = horaToDecimal( hFin) * 2
     h2 = horaToDecimal( hIni) * 2
     if(h1 <= h2):
@@ -125,6 +126,7 @@ def noEstaSolapado(hFin,hIni):
         return(False)
 
 def horaToDecimal(hora):
+    # Convierte una hora en formato (HH:MM) a un numero 
     hIniDateObject = datetime.timedelta(hours= int(hora[:hora.find(':')]),minutes= int(hora[hora.find(':')+1:]))
     decimalHora = hIniDateObject.total_seconds() / 3600
     return decimalHora        
@@ -140,9 +142,10 @@ def valorPeso(a):
 
 if __name__ == "__main__":
     # formato: beneficio = [[valor, peso], ..., n]
-    leeArchivo()
+    src = input("Ingrese la ruta del archivo: ")
+    leeArchivo(src)
     matrizOrdenada = ordenarMatriz(entrada,2)
     beneficio = valorPeso(matrizOrdenada)
     W = 48
-    print('Solucion: ',usoSalaCirugias(W, beneficio, matrizOrdenada) )
+    #print('Solucion: ',usoSalaCirugias(W, beneficio, matrizOrdenada) )
     crearArchivoSalida(usoSalaCirugias(W, beneficio, matrizOrdenada))
